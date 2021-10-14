db89b2ca5b4d24af289c04ed324eef83ce45700c
Tuan Coder <tuandang29042000@gmail.com>
2021-10-14 11:05:29 +0700

    hoan thanh chuc nang GetUserById

---
+++ eComSolution.ViewModel/Common/ApiResult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.ViewModels.Common
+{
+    public class ApiResult<T>
+    {
+        public bool IsSuccessed { get; set; }
+
+        public string Message { get; set; }
+
+        public T ResultObj { get; set; }
+
+        public ApiResult(bool IsSuccessed)
+        {
+            this.IsSuccessed = IsSuccessed;
+        }
+        public ApiResult(bool IsSuccessed, string Message)
+        {
+            this.IsSuccessed = IsSuccessed;
+            this.Message = Message;
+        }
+        public ApiResult(bool IsSuccessed, T ResultObj)
+        {
+            this.IsSuccessed = IsSuccessed;
+            this.ResultObj = ResultObj;
+        }
+    }
+}
