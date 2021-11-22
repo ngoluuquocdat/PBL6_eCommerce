0866aa1a0e13d52cdf772673309b8109ff840f04
Quoc Dat <ngoluuquocdat@gmail.com>
2021-11-22 08:00:03 +0700

    sua view model + them validation

--- eComSolution.ViewModel/Catalog/Products/CreateProductRequest.cs
+++ eComSolution.ViewModel/Catalog/Products/CreateProductRequest.cs
@@ -20,9 +20,36 @@ namespace eComSolution.ViewModel.Catalog.Products
         public int CategoryId { get; set; }
         public int ShopId { get; set; }
         //public bool IsDeleted { get; set; }
-        public List<ProductDetailVm> Details {get; set;}
+        public List<CreateProductDetailVm> Details {get; set;}
         //public List<CreateProductImagesRequest> NewImages { get; set; }
         // public List<ImageInfo> ImageInfos {get; set;}
         // public List<IFormFile> NewImages {get; set;}
+        public bool IsValid()
+        {
+            // null or empty check
+            if(String.IsNullOrEmpty(Name) || String.IsNullOrEmpty(Description)
+                        || Gender==0 || Price==0 || OriginalPrice==0 || CategoryId==0 || ShopId==0)
+            {
+                return false;
+            }
+            // valid gender 
+            if(Gender!=1 && Gender!=2 && Gender!= 3)
+            {
+                return false;
+            }
+            // valid price & original price
+            if(Price<0 || OriginalPrice<0)
+            {
+                return false;
+            }
+            // valid product details
+            foreach(var detail in Details)
+            {
+                if(detail.IsValid()==false)
+                    return false;
+            }
+
+            return true;
+        }
     }
 }
